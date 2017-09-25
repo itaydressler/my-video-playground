76577da9dfa27dc3f39f00f2149f92f325a60348
Itay Dressler <itaydressler@gmail.com>
2017-09-25 11:39:34 +0300

    Added some mongo db tests.

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+var mongoose = require('mongoose');
+const mongoUrl = process.env.MONGODB_URI;
+
+if (mongoUrl) {
+	console.log("Connecting to " + mongoUrl);
+	mongoose.connect(mongoUrl, err => console.log('Error connecting = ' + err));
+}
+else {
+	console.log('Np mongo url detected');
+}
